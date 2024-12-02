2a2ff5944fe4d60c323e4fdb5f39d27540eaa458
yneo918 <yneo918@gmail.com>
2024-12-02 11:38:17 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,2 @@
-pip install adafruit-circuitpython-gps --break-system-packages
+sudo apt-get update && sudo apt-get install raspi-config i2c-tools -y 
+pip install adafruit-circuitpython-gps RPi.GPIO adafruit-circuitpython-bno055 --break-system-packages
